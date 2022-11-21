357d50546ebef521917f43cca4fe9a9114f9a8bf
RaRseR <86536250+RaRseR@users.noreply.github.com>
2022-11-22 00:52:22 +0500

    Update FlattenBinaryTreeToLinkedList.go

--- FlattenBinaryTreeToLinkedList.go
+++ FlattenBinaryTreeToLinkedList.go
@@ -1,1 +1,16 @@
+func flatten(root *TreeNode) {
+	var helper func(root *TreeNode)
 
+	var node *TreeNode
+	helper = func (root *TreeNode) {
+		if root == nil {
+			return
+		}
+		helper(root.Right)
+		helper(root.Left)
+		root.Right = node
+		root.Left = nil
+		node = root
+	}
+	helper(root)
+}
