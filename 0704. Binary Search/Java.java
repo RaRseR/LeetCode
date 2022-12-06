d6b91ecf6fe774e6a278908923f566f8553633c6
RaRseR <86536250+RaRseR@users.noreply.github.com>
2022-12-07 01:29:13 +0500

    Update Java.java

--- 0704. Binary Search/Java.java
+++ 0704. Binary Search/Java.java
@@ -1,17 +1,15 @@
-class Solution {
-    public int search(int[] nums, int target) {
-        int left = 0;
-        int right =  nums.length - 1;
-        while (left <= right) {
-            int middle = (left + right) / 2;
-            if (nums[middle] == target) {
-                return middle;
-            } else if (nums[middle] < target) {
-                left = middle + 1;
-            }  else {
-                right = middle - 1;
-            }
+public int search(int[] nums, int target) {
+    int left = 0;
+    int right =  nums.length - 1;
+    while (left <= right) {
+        int middle = (left + right) / 2;
+        if (nums[middle] == target) {
+            return middle;
+        } else if (nums[middle] < target) {
+            left = middle + 1;
+        }  else {
+            right = middle - 1;
         }
-        return -1;
     }
+    return -1;
 }
