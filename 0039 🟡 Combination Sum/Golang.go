ed2f8c7e9b310b46186ba9f05f7511553d2d6870
RaRseR <86536250+RaRseR@users.noreply.github.com>
2022-12-07 20:08:02 +0500

    Create Golang.go

---
+++ 0039 🟡 Combination Sum/Golang.go
@@ -0,0 +1,28 @@
+func combinationSum(candidates []int, target int) [][]int {
+    sort.Ints(candidates)
+    
+    result := [][]int{}
+    
+    helper(candidates, target, []int{}, &result)
+    
+    return result
+}
+
+
+func helper(n []int, t int, p []int, r *[][]int){
+    if t < 0 {
+       return
+    }
+    
+    if t == 0 {
+        c := make([]int, len(p))
+        copy(c, p)
+        
+        *r = append(*r, c)
+        return
+    }
+        
+    for i, v := range n  {
+        helper(n[i:], t - v, append(p, v), r)
+    }
+}
