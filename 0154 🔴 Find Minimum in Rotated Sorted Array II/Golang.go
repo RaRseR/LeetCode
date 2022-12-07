b175967ae530c6662f49687d47d35b81df7f59d5
RaRseR <86536250+RaRseR@users.noreply.github.com>
2022-12-08 00:41:02 +0500

    Update Golang.go

--- 0154 🔴 Find Minimum in Rotated Sorted Array II/Golang.go
+++ 0154 🔴 Find Minimum in Rotated Sorted Array II/Golang.go
@@ -1,3 +1,21 @@
 func findMin(nums []int) int {
-    
+    if len(nums) == 1 {
+        return nums[0]
+    }
+    l := 0
+    r := len(nums) - 1
+
+    for l < r {
+        m := (l + r) / 2
+        if nums[m] > nums[r] {
+            l = m + 1
+        } else if nums[l] == nums[r]{
+            l++
+            r--
+        } else {
+            r = m
+        }
+    }
+
+    return nums[l]
 }
