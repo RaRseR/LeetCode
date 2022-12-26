da38971d70e48ebaeb4c3a36f7bf7599637a5836
RaRseR <86536250+RaRseR@users.noreply.github.com>
2022-12-26 19:02:25 +0500

    Create Golang.go

---
+++ 1046 🟢 Last Stone Weight/Golang.go
@@ -0,0 +1,27 @@
+func lastStoneWeight(stones []int) int {
+    h := (*IntHeap)(&stones)
+	heap.Init(h)
+    
+	for h.Len() > 1 {
+        left := heap.Pop(h).(int) - heap.Pop(h).(int)
+        if left > 0{
+            heap.Push(h, left)
+        }
+	}
+    
+    if h.Len() == 1 {
+        return heap.Pop(h).(int)
+	}
+    
+    return 0
+}
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
