d39fa2356299cae8cfed4c129e1913cfcb052f1a
RaRseR <86536250+RaRseR@users.noreply.github.com>
2023-02-22 02:14:59 +0500

    Update Golang.go

--- 0989 🟢 Add to Array-Form of Integer/Golang.go
+++ 0989 🟢 Add to Array-Form of Integer/Golang.go
@@ -1,22 +1,21 @@
 func searchInsert(nums []int, target int) int {		
-	n := len(nums)
 	l := 0
-	r := n - 1
+	r := len(nums) - 1
 
 	for l < r {
-	m := l + (r - l) / 2
+	  m := l + (r - l) / 2
 			
 		if nums[m] == target {
-			return m
-        } else if nums[m] > target {
-            r = m
-        } else {
-            l = m + 1
-        }
-	}
-	
-    if nums[l] < target {
-        return l + 1
+      return m
+    } else if nums[m] > target {
+      r = m
+    } else {
+      l = m + 1
     }
-    return l
+  }
+	
+  if nums[l] < target {
+    return l + 1
+  }
+  return l
 }
