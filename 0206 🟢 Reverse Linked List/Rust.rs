33b61f2d406e798634a1733576a71c178cceeb17
Ruslan <86536250+RaRseR@users.noreply.github.com>
2023-06-06 02:46:26 +0500

    Create Rust.rs

---
+++ 0206 🟢 Reverse Linked List/Rust.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut prev = None;
+        let mut curr = head;
+
+        while let Some(mut node) = curr  {
+            curr = node.next;
+      
+            node.next = prev;
+            
+            prev = Some(node);
+        }
+        
+        prev
+    }
+}
