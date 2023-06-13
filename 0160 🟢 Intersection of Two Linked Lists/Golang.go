48812f21e90f65eb2830e9a7d9a422b23d4d37ad
Ruslan <86536250+RaRseR@users.noreply.github.com>
2023-06-13 20:04:12 +0500

    Create Golang.go

---
+++ 0160 🟢 Intersection of Two Linked Lists/Golang.go
@@ -0,0 +1,20 @@
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+    var a = headA
+    var b = headB
+
+    for a != b {
+        if a == nil {
+            a = headB
+        } else {
+            a = a.Next
+        }
+
+        if b == nil {
+            b = headA
+        } else {
+           b = b.Next
+        }
+    }
+
+    return a
+}
