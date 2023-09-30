f08d4691dd54d9070dbccc8ec44b31be1a939b9f
Ruslan Nurmukhametov <86536250+RaRseR@users.noreply.github.com>
2023-09-30 14:06:01 +0500

    Update Golang.go

--- 0076 🔴 Minimum Window Substring/Golang.go
+++ 0076 🔴 Minimum Window Substring/Golang.go
@@ -6,9 +6,9 @@ func minWindow(s string, t string) string {
     targetCharacterFrequency := make(map[uint8]int)
     currentCharacterFrequency := make(map[uint8]int)
 
-	for index := range t {
+    for index := range t {
 		targetCharacterFrequency[t[index]]++
-	}
+    }
 
     leftPointer, rightPointer := 0, 0
     distinctCharacterCount := 0
@@ -17,20 +17,20 @@ func minWindow(s string, t string) string {
     for rightPointer < len(s) {
         currentCharacterFrequency[s[rightPointer]]++
         if targetCharacterFrequency[s[rightPointer]] != 0 &&
-			targetCharacterFrequency[s[rightPointer]] == currentCharacterFrequency[s[rightPointer]] {
-			distinctCharacterCount++
-		}
+	    	targetCharacterFrequency[s[rightPointer]] == currentCharacterFrequency[s[rightPointer]] {
+				distinctCharacterCount++
+	    }
 
         for distinctCharacterCount == len(targetCharacterFrequency) {
             if result == "" || rightPointer - leftPointer + 1 < len(result) {
                 result = s[leftPointer:rightPointer + 1]
             }
             
-            currentCharacterFrequency[s[leftPointer]]--
+			currentCharacterFrequency[s[leftPointer]]--
 			if currentCharacterFrequency[s[leftPointer]] < targetCharacterFrequency[s[leftPointer]] {
 				distinctCharacterCount--
 			}
-
+			
 			leftPointer++
         }
         
