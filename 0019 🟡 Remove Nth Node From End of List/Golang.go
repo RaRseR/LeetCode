2959818b71e7e1946640390edee669fd5e553df2
Ruslan Nurmukhametov <86536250+RaRseR@users.noreply.github.com>
2023-10-26 00:49:14 +0500

    Create Golang.go

---
+++ 0019 🟡 Remove Nth Node From End of List/Golang.go
@@ -0,0 +1,19 @@
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+    slow, fast := head, head
+    for n > 0 {
+        fast = fast.Next
+        n--
+    }
+
+    if fast == nil {
+        return slow.Next
+    }
+
+    for fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next
+    }
+    
+    slow.Next = slow.Next.Next
+    return head
+}
