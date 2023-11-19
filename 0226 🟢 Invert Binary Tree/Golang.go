a8de942d7b67783e82c1e4e256ff8335a62ca1c5
Ruslan Nurmukhametov <86536250+RaRseR@users.noreply.github.com>
2023-11-20 00:49:15 +0500

    Update Golang.go

--- 0226 🟢 Invert Binary Tree/Golang.go
+++ 0226 🟢 Invert Binary Tree/Golang.go
@@ -1,9 +1,7 @@
 func invertTree(root *TreeNode) *TreeNode {
-    if root == nil {
-        return nil
+    if root != nil {
+        root.Left, root.Right = invertTree(root.Right), invertTree(root.Left) 
     }
-
-    root.Left, root.Right = invertTree(root.Right), invertTree(root.Left) 
 
     return root
 }
