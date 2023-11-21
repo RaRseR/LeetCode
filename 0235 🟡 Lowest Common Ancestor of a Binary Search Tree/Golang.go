299fae595d24975acc8848f73cf3f82fdb1577c1
Ruslan Nurmukhametov <86536250+RaRseR@users.noreply.github.com>
2023-11-21 21:59:55 +0500

    Create Golang.go

---
+++ 0235 🟡 Lowest Common Ancestor of a Binary Search Tree/Golang.go
@@ -0,0 +1,13 @@
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    for root != nil {
+        if p.Val > root.Val && q.Val > root.Val {
+            root = root.Right
+        } else if p.Val < root.Val && q.Val < root.Val {
+            root = root.Left
+        } else {
+            break
+        }
+    }
+
+    return root
+}
