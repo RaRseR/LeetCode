747a9eba495a96165981de98f39520b0d0dd7bb9
Ruslan Nurmukhametov <86536250+RaRseR@users.noreply.github.com>
2023-11-26 12:16:38 +0500

    Update Golang.go

--- 1685 🟡 Sum of Absolute Differences in a Sorted Array/Golang.go
+++ 1685 🟡 Sum of Absolute Differences in a Sorted Array/Golang.go
@@ -9,11 +9,8 @@ func getSumAbsoluteDifferences(nums []int) (result []int) {
     for i := 0; i < len(nums); i++ {
         var rightSum int = totalSum - leftSum - nums[i]
 
-        var leftCount int = i
-        var rightCount int = len(nums) - i - 1
-
-        var leftTotal int = leftCount * nums[i] - leftSum
-        var rightTotal int = rightSum - rightCount * nums[i]
+        var leftTotal int = i * nums[i] - leftSum
+        var rightTotal int = rightSum - (len(nums) - i - 1) * nums[i]
 
         result = append(result, leftTotal + rightTotal)
         leftSum += nums[i]
