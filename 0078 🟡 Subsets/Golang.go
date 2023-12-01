3ca37265e61316635601c1ae0f9904a03051c5d7
Ruslan Nurmukhametov <86536250+RaRseR@users.noreply.github.com>
2023-12-01 17:59:37 +0500

    Update Golang.go

--- 0078 🟡 Subsets/Golang.go
+++ 0078 🟡 Subsets/Golang.go
@@ -1,22 +1,21 @@
 func subsets(nums []int) (result [][]int) {
-    sort.Ints(nums)
+	current := make([]int, 0)
 
-	for i := 0; i <= len(nums); i++ {
-		helper(nums, i, 0, []int{}, &result)
+  var backtrack func(idx int)
+  backtrack = func(idx int) {
+		result = append(result, append([]int{}, current...))
+
+		if idx == len(nums) {
+			return
+		}
+
+		for i := idx; i < len(nums); i++ {
+			current = append(current, nums[i])
+			backtrack(i + 1)
+			current = current[:len(current)-1]
+		}
 	}
+	backtrack(0)
 
-	return result
-}
-
-func helper(n []int, i int, s int, p []int, r *[][]int) {
-	if len(p) == i {
-		*r = append(*r, append([]int{}, p...))
-		return
-	}
-
-	for j := s; j <= len(n) - (i - len(p)); j++ {
-		p = append(p, n[j])
-		helper(n, i, j + 1, p, r)
-		p = p[:len(p) - 1]
-	}
+  return
 }
