45d499897d44e477402c4db59618e12c6701d6bd
Ruslan Nurmukhametov <86536250+RaRseR@users.noreply.github.com>
2023-12-23 19:13:22 +0500

    Create Bash.sh

---
+++ 0192 🟡 Word Frequency/Bash.sh
@@ -0,0 +1,11 @@
+declare -A freq
+
+while read -r -a words; do
+  for word in "${words[@]}"; do
+    ((freq[$word]++))
+  done
+done <words.txt
+
+for word in "${!freq[@]}"; do
+  echo "$word ${freq[$word]}"
+done | sort -k2 -n -r
