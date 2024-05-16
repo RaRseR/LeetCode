7de28c67f68ab8c6898740c9d09f966c6b6c7fe9
Ruslan Nurmukhametov <86536250+RaRseR@users.noreply.github.com>
2024-05-16 18:31:10 +0500

    Create Golang.go

---
+++ 2331 🟢 Evaluate Boolean Binary Tree/Golang.go
@@ -0,0 +1,14 @@
+func evaluateTree(root *TreeNode) bool {
+    if root.Left == nil {
+        return root.Val == 1
+    }
+
+    left := evaluateTree(root.Left)
+    right := evaluateTree(root.Right)
+
+    if root.Val == 2 {
+        return left || right
+    }
+
+    return left && right
+}
